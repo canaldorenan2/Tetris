522a4426240935fcc3c9613311c25063890c3e42
Renan Cascarano de Souza <30605188+canaldorenan2@users.noreply.github.com>
2021-06-30 10:03:43 -0300

    Primeira versão da playstore

---
+++ Assets/Scripts/BannerAD.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GoogleMobileAds.Api;
+
+public class BannerAD : MonoBehaviour
+{
+
+    private BannerView bannerView;
+
+    void Start()
+    {
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+
+        this.RequestBanner();
+    }
+
+    public void RequestBanner()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-7216001829525156/4947676322";
+        //string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Tetris");
     }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
 }
